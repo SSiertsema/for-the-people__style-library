6b82747e3ea0c8f3884075600485a444973e7f3d
Sven Siertsema <sven.siertsema@gmail.com>
2023-10-21 07:51:54 +0200

    adds new breakpoint

--- src/components/Grid/Grid.vue
+++ src/components/Grid/Grid.vue
@@ -11,23 +11,6 @@
   </div>
 </template>
 
-<script setup>
-const headingVariants = ["xxxl", "xxl", "xl", "l", "m", "s"];
-const headingSnippet = `The fox jumps over the lazy brown dog`;
-
-const textVariants = ["l", "m", "s"];
-const textSnippet = `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-  nisi ut aliquip ex ea commodo consequat.
-  <strong>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-  dolore eu fugiat nulla pariatur.</strong>
-  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-  officia deserunt mollit anim id est laborum.
-`;
-</script>
-
 <style lang="scss">
 @import "../../scss/variables/spacers.scss";
 @import "../../scss/fonts.scss";
